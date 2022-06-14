08df339eabdfbc5f32dfe253fa7788bf9022fd90
Fawad Ali <m.fawaadali98@gmail.com>
2022-06-15 03:40:32 +0500

    Update dependencies and use better tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { openFile, draw } from 'jsroot';
 
-openFile('https://root.cern/js/files/hsimple.root').then((file) => {
-  file.readObject('hpxpy;1').then((obj) => {
-    draw('drawing', obj, 'colz');
-  });
-});
+async function drawWithJSROOT() {
+  let file = await openFile('https://root.cern/js/files/hsimple.root');
+  let obj = await file.readObject('hpxpy;1');
+
+  draw('drawing', obj, 'colz');
+}
+
+drawWithJSROOT();
