e5af432b11754a6c06197f720a48e1b5a19bb1a4
Fawad Ali <m.fawaadali98@gmail.com>
2022-06-14 22:12:36 +0500

    Example usage of JSROOT

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import { openFile, draw } from 'jsroot';
 
+openFile('https://root.cern/js/files/hsimple.root').then((file) => {
+  file.readObject('hpxpy;1').then((obj) => {
+    draw('drawing', obj, 'colz');
+  });
+});
