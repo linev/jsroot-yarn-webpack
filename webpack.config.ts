3b426b1d1fc17fdc8da4e31f7cb6a15c1c298b49
Sergey Linev <S.Linev@gsi.de>
2022-06-15 15:46:16 +0200

    adjust config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import { Configuration } from 'webpack';
+import { Configuration, DefinePlugin } from 'webpack';
 
 const config: Configuration = {
   mode: 'development',
@@ -12,8 +12,37 @@ const config: Configuration = {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
+  experiments: {
+    topLevelAwait: true
+  },
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
+    extensions: ['.ts', '.js', '.json', '.mjs'],
+    fallback: {
+      "fs" : false,
+      "crypto": false,
+      "zstd-codec": false,
+      "zlib": false,
+      "stream": false,
+      "url": false,
+      "path": false,
+      "util": false,
+      "http": false,
+       "https": false,
+       "os": false,
+       "net" : false,
+       "tls": false,
+       "assert": false,
+       "atob": false,
+       "btoa": false,
+       "canvas": false,
+       "gl": false,
+       "jsdom": false,
+       "mathjax": false,
+       "tmp": false,
+       "xhr": false,
+       "xhr2": false,
+       "child_process": false
+    }
   },
   module: {
     rules: [
@@ -31,6 +60,10 @@ const config: Configuration = {
       template: path.resolve(__dirname, './public/index.html'),
     }),
     new ForkTsCheckerWebpackPlugin(),
+    new DefinePlugin({
+       "process.env": {
+          APP_ENV: JSON.stringify('browser')
+      }})
   ],
 };
 
