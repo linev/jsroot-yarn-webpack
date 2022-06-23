1585f2e6a6a51e1f6ea0a322288ee649454b72f7
Sergey Linev <S.Linev@gsi.de>
2022-06-23 13:57:01 +0200

    Add example how HierarchyPainter can be used

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-import { openFile, draw } from 'jsroot';
+import { openFile, draw, HierarchyPainter, setDefaultDrawOpt } from 'jsroot';
 
 async function drawWithJSROOT() {
   let file = await openFile('https://root.cern/js/files/hsimple.root');
   let obj = await file.readObject('hpxpy;1');
 
-  draw('drawing', obj, 'colz');
+  draw('drawDiv', obj, 'colz');
 }
 
-drawWithJSROOT();
+// drawWithJSROOT();
+
+async function drawWithHPainter() {
+
+   // change default draw option for supported ROOT class,
+   // used when object clicked in the hierarchy
+   setDefaultDrawOpt('TH1', 'text');
+   setDefaultDrawOpt('TH2', 'col');
+
+   let h = new HierarchyPainter("example", "hierarchyDiv");
+
+   // configure 'simple' in provided <div> element
+   // one also can specify "grid2x2" or "flex"
+   h.setDisplay("simple", "drawDiv");
+
+   // open file and display element
+   await h.openRootFile("https://root.cern/js/files/hsimple.root");
+
+   await h.display("hpxpy;1", "colz");
+}
+
+drawWithHPainter();
